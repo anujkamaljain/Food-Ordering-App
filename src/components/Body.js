ac68b4942a4951a97c587be4985ac3946b2ecbbb
anujkamaljain <23UCS540@lnmiit.ac.in>
2025-01-21 18:16:29 +0530

    Readme changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,7 +15,7 @@ const Body = () => {
 
   const fetchData = async () => {
       const Data = await fetch(
-        "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9195866&lng=75.7879608&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://gobetween.oklabs.org/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9195866&lng=75.7879608&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
 
       const json = await Data.json();
